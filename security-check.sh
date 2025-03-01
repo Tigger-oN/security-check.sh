9c34932894bed59b3b3452e9c12e73a45c3fa63a
Tigger-oN <tigger@lvlworld.com>
2025-03-01 19:29:01 +1100

    Support for Linux and FreeBSD environments

--- security-check.sh
+++ security-check.sh
@@ -3,13 +3,11 @@
 # Present the last records in the FreeBSD Security Advisories and
 # Errata notices RSS
 #
-# TODO: 
-# - Started work on support for Linux, finish or remove it?
 
 # RSS location
 URL="https://www.freebsd.org/security/feed.xml"
 # Script version
-VERSION="20250228"
+VERSION="20250301"
 # Used with date formating
 DATE_TMP=""
 # Check if an item is new or not
@@ -35,25 +33,36 @@ Script version: ${VERSION}
 }
 
 # For some odd reason, the feed has more than one date format.
-# `date` is OS dependent, so we only do corrections for FreeBSD.
+# `date` options we need are OS dependent.
 dateFormat () {
 	NEW_ITEM=""
-	if [ -z "${1}" -o "${OS}" != "FreeBSD" ]
+	if [ -z "${1}" ]
 	then
 		DATE_TMP="${1}"
 		return
 	fi
-	# Which version are we correcting?
-	numb_only=`printf "%s" "${1}" | grep -o "[0-9-]\{10\}"`
-	if [ -n "${numb_only}" ]
+	if [ "${OS}" = "FreeBSD" ]
 	then
-		# Input is like 2025-02-28
-		DATE_TMP=`date -j -f "%Y-%m-%d" "${1}" "+%e %b, %Y" 2> /dev/null`
-		tmp=`date -j -f "%Y-%m-%d %H:%M:%S" "${1} 00:00:00" "+%s" 2> /dev/null`
+		# Which date format version are we correcting?
+		numb_only=`printf "%s" "${1}" | grep -o "[0-9-]\{10\}"`
+		if [ -n "${numb_only}" ]
+		then
+			# Input is like 2025-02-28
+			DATE_TMP=`date -j -f "%Y-%m-%d" "${1}" "+%e %b, %Y" 2> /dev/null`
+			tmp=`date -j -f "%Y-%m-%d %H:%M:%S" "${1} 00:00:00" "+%s" 2> /dev/null`
+		else
+			# Input is like 28 Feb 25
+			DATE_TMP=`date -j -f "%d %b %y" "${1}" "+%e %b, %Y" 2> /dev/null`
+			tmp=`date -j -f "%d %b %y %H:%M:%S" "${1} 00:00:00" "+%s" 2> /dev/null`
+		fi
+	elif [ "${OS}" = "Linux" ]
+	then
+		# Linux has `date -d` which handles both feed date formats
+		DATE_TMP=`date -d "${1}" "+%e %b, %Y" 2> /dev/null`
+		tmp=`date -d "${1} 00:00:00" "+%s" 2> /dev/null`
 	else
-		# Input is like 28 Feb 25
-		DATE_TMP=`date -j -f "%d %b %y" "${1}" "+%e %b, %Y" 2> /dev/null`
-		tmp=`date -j -f "%d %b %y %H:%M:%S" "${1} 00:00:00" "+%s" 2> /dev/null`
+		DATE_TMP="${1}"
+		return
 	fi
 	if [ -n "${tmp}" -a ${LAST_CHECK_DATE} -lt "${tmp}" ]
 	then
@@ -77,10 +86,10 @@ getFeed () {
 		FEED=`fetch -qo - "${URL}"`
 	else
 		# Might be on Linux, need to check
-		if [ -n `command -v wget` ]
+		if [ -n "`command -v wget`" ]
 		then
 			FEED=`wget -qO - "${URL}"`
-		elif [ -n `command -v curl` ]
+		elif [ -n "`command -v curl`" ]
 		then
 			FEED=`curl -so - "${URL}"`
 		else
@@ -101,15 +110,36 @@ Try again or check your network status"
 	fi
 }
 
+checkFeed () {
+	header=`printf "%s" "${FEED}" | grep -o "<rss version=\"2.0\""`
+	footer=`printf "%s" "${FEED}" | grep -o "</rss>"`
+	if [ "${header}" != "<rss version=\"2.0\"" -o "${footer}" != "</rss>" ]
+	then
+		printf "\nThe downloaded feed appears to be invalid. Recommend checking the network\nconnection. If everything appears to be correct, please try again.\n\nIf the problem continues, manually check the data at:\n\n %s\n\n" "${URL}"
+		exit
+	fi
+}
+
 limitTo3Months () {
-	# Limit to last 3 months AND support both date formats.
-	# Why are two different date formats used?
-	D1=`date "+%b %y"`
-	D2=`date -v-1m "+%b %y"`
-	D3=`date -v-2m "+%b %y"`
-	D1a=`date "+%Y-%m"`
-	D2a=`date -v-1m "+%Y-%m"`
-	D3a=`date -v-2m "+%Y-%m"`
+	# Limit to last 3 months and support both date formats used in the feed.
+	if [ "${OS}" = "FreeBSD" ]
+	then
+		# Possible Darwin would be the same (unconfirmed)
+		D1=`date "+%b %y"`
+		D2=`date -v-1m "+%b %y"`
+		D3=`date -v-2m "+%b %y"`
+		D1a=`date "+%Y-%m"`
+		D2a=`date -v-1m "+%Y-%m"`
+		D3a=`date -v-2m "+%Y-%m"`
+	elif [ "${OS}" = "Linux" ]
+	then
+		D1=`date "+%b %y"`
+		D2=`date -d "last month" "+%b %y"`
+		D3=`date -d "2 months ago" "+%b %y"`
+		D1a=`date "+%Y-%m"`
+		D2a=`date -d "last month" "+%Y-%m"`
+		D3a=`date -d "2 months ago" "+%Y-%m"`
+	fi
 
 	# Clean and sort the feed.
 	RAW=`printf "%s" "${FEED}" | grep -io "<item>\|<\/item>\|<title>.*<\/title>\|<link>.*<\/link>\|<pubDate>.*<\/pubDate>" | grep -A3 "<item>" | tr -d "\n" | sed 's/--<item>/\n/g; s/<item>//; s/ 00:00 UTC//g' | sort -r | grep "${D1}\|${D2}\|${D3}\|${D1a}\|${D2a}\|${D3a}"`
@@ -126,14 +156,11 @@ then
 	usage
 fi
 
-if [ "${OS}" != "FreeBSD" ]
-then
-	printf "\nThis script has been designed to run on FreeBSD only.\n\n"
-	exit
-fi
-
 # Get the raw feed
 getFeed
+
+# Make sure we the feed is valid
+checkFeed
 
 # Limit to 3 months
 limitTo3Months
